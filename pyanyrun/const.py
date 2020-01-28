0271817189cadca23a329f45d5148b5e6fe6a4e1
samsepi01 <33626923+er28-0652@users.noreply.github.com>
2020-01-28 10:40:42 +0900

    update: change const data structure

--- pyanyrun/const.py
+++ pyanyrun/const.py
@@ -1,28 +1,43 @@
 import typing as t
 from typing_extensions import Literal
+from dataclasses import dataclass
 
-T_RUN_TYPES = t.List[Literal['url', 'file']]
-RUN_TYPES = {
-    'url': '0',
-    'file': '1'
-}
 
-T_EXTENSIONS = t.List[Literal['exe', 'dll', 'java', 'html', 'flash', 'pdf', 'office', 'script', 'email']]
-EXTENSIONS = {
-    'exe': '0',
-    'dll': '1',
-    'java': '2',
-    'html': '3',
-    'flash': '4',
-    'pdf': '5',
-    'office': '6',
-    'script': '7',
-    'email': '8'
-}
 
-T_VERDICTS = t.List[Literal['malicious', 'normal', 'no-threats']]
-VERDICTS = {
-    'malicious': 2,
-    'normal': 1,
-    'no-threats': 0
-}
+@dataclass
+class AnyRunConsts:
+    types: t.Any
+    data: dict
+
+
+RUN_TYPES = AnyRunConsts(
+    types=t.List[Literal['url', 'file']],
+    data={
+        'url': '0',
+        'file': '1'
+    }
+)
+
+EXTENSIONS = AnyRunConsts(
+    types=t.List[Literal['exe', 'dll', 'java', 'html', 'flash', 'pdf', 'office', 'script', 'email']],
+    data={
+        'exe': '0',
+        'dll': '1',
+        'java': '2',
+        'html': '3',
+        'flash': '4',
+        'pdf': '5',
+        'office': '6',
+        'script': '7',
+        'email': '8'
+    }
+)
+
+VERDICTS = AnyRunConsts(
+    types=t.List[Literal['malicious', 'normal', 'no-threats']],
+    data={
+        'malicious': 2,
+        'normal': 1,
+        'no-threats': 0
+    }
+)
