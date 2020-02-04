0f95b302b19cb2a89d6eecf6450dfbe99d9087c8
samsepi01 <33626923+er28-0652@users.noreply.github.com>
2020-02-05 03:32:09 +0900

    update: printable class

--- aio_anyrun/collection.py
+++ aio_anyrun/collection.py
@@ -1,3 +1,4 @@
+import json
 import typing as t
 from datetime import datetime
 from dataclasses import dataclass
@@ -5,19 +6,33 @@ from dataclasses import dataclass
 from aio_anyrun import const as cst
 
 
-class Task:
-    def __init__(self, task_info: dict):
-        self._info = task_info
+class BaseCollection:
+    def __init__(self, raw_data: dict):
+        self.raw_data = raw_data
+        self._ignores = ['items', 'json', 'raw_data', 'keys', 'values']
+        self.properties = [prop for prop in dir(self) if not prop.startswith('_') and prop not in self._ignores]
+    
+    def json(self):
+        return json.dumps(self.raw_data, indent=4)
     
     def __str__(self):
-        return f'Task(run_type={self.run_type}, name={self.name}, verdict={self.verdict}, task_uuid={self.task_uuid}, object_uuid={self.object_uuid})'
-    
+        return f'{self.__name__}({ ", ".join([f"{k}={v}" for k, v in self.items()]) })'
+
     def __repr__(self):
         return self.__str__()
+
+    def __getitem__(self, key):
+        return self.raw_data.get(key)
     
+    def items(self):
+        for prop in self.properties:
+            yield prop, getattr(self, prop)
+
+
+class Task(BaseCollection):    
     @property
     def threat_level(self) -> int:
-        return self._info['scores']['verdict']['threat_level']
+        return self.raw_data['scores']['verdict']['threat_level']
     
     @property
     def verdict(self) -> str:
@@ -29,23 +44,23 @@ class Task:
     
     @property
     def tags(self) -> t.List[str]:
-        return self._info['tags']
+        return self.raw_data['tags']
     
     @property
     def task_uuid(self) -> str:
-        return self._info['uuid']
+        return self.raw_data['uuid']
     
     @property
     def os_version(self) -> dict:
-        return self._info['public']['environment']['OS']
+        return self.raw_data['public']['environment']['OS']
     
     @property
     def run_type(self) -> str:
-        return self._info['public']['objects']['runType']
+        return self.raw_data['public']['objects']['runType']
     
     @property
     def main_object(self) -> dict:
-        return self._info['public']['objects']['mainObject']
+        return self.raw_data['public']['objects']['mainObject']
     
     @property
     def hashes(self) -> dict:
@@ -117,61 +132,58 @@ REPUTATION_TABLE: t.Dict[int, str] = {
     4: 'unsafe'
 }
 
-@dataclass
-class IoCObject:
-    category: str = ''
-    type: str = ''
-    ioc: str = ''
-    reputation: StrOrInt = ''
-    name: str = ''
+class IoCObject(BaseCollection):
+    @property
+    def category(self):
+        return self.raw_data.get('category')
     
-    def __post_init__(self):
-        if self.reputation:
-            self.reputation = REPUTATION_TABLE[t.cast(int, self.reputation)]
+    @property
+    def types(self):
+        return self.raw_data.get('type')
+    
+    @property
+    def ioc(self):
+        return self.raw_data.get('ioc')
+    
+    @property
+    def reputation(self):
+        return REPUTATION_TABLE[self.raw_data['reputation']]
+    
+    @property
+    def name(self):
+        return self.raw_data.get('name')
 
 
-class IoC:
+class IoC(BaseCollection):
     ''' Class to represent IoC information.
-    '''
-    def __init__(self, ioc: dict):
-        self.ioc = ioc
-    
-    def __str__(self) -> str:
-        return f'IoC(main_object={self.main_objects}, dropped_files={self.dropped_files}, dns={self.dns}, connections={self.connections})'
-    
-    def __repr__(self) -> str:
-        return self.__str__()
-    
+    '''        
     @staticmethod
     def _parse(obj: t.Optional[dict]) -> t.List[IoCObject]:
         if obj is None:
-            return [IoCObject()]
-        return [IoCObject(**o) for o in obj]
+            return []
+        return [IoCObject(o) for o in obj]
     
     @property
     def main_objects(self) -> t.List[IoCObject]:
-        return self._parse(self.ioc['Main object'])
+        return self._parse(self.raw_data['Main object'])
     
     @property
     def dropped_files(self) -> t.List[IoCObject]:
-        return self._parse(self.ioc.get('Dropped executable file'))
+        return self._parse(self.raw_data.get('Dropped executable file'))
     
     @property
     def dns(self) -> t.List[IoCObject]:
-        return self._parse(self.ioc.get('DNS requests'))
+        return self._parse(self.raw_data.get('DNS requests'))
     
     @property
     def connections(self) -> t.List[IoCObject]:
-        return self._parse(self.ioc.get('Connections'))
+        return self._parse(self.raw_data.get('Connections'))
 
 
-class MITRE_Attack:
-    def __init__(self, mitre_data: dict):
-        self.data = mitre_data
-    
+class MITRE_Attack(BaseCollection):    
     @property
-    def _external_references(self) -> t.List[dict]:
-        return self.data['external_references']
+    def _external_references(self) -> t.Optional[t.List[dict]]:
+        return self.raw_data.get('external_references')
     
     @property
     def mitre_url(self) -> t.Optional[str]:
@@ -181,33 +193,34 @@ class MITRE_Attack:
         return ''
     
     @property
-    def technique(self) -> str:
-        return self.data['technique']
+    def technique(self) -> t.Optional[str]:
+        return self.raw_data.get('technique')
     
     @property
-    def name(self) -> str:
-        return self.data['name']
+    def name(self) -> t.Optional[str]:
+        return self.raw_data.get('name')
     
     @property
-    def mitre_detection(self) -> str:
-        return self.data['x_mitre_detection']
+    def mitre_detection(self) -> t.Optional[str]:
+        return self.raw_data.get('x_mitre_detection')
     
     @property
-    def platforms(self) -> t.List[str]:
-        return self.data['x_mitre_platforms']
+    def platforms(self) -> t.Optional[t.List[str]]:
+        return self.raw_data.get('x_mitre_platforms')
     
     @property
-    def kill_chain_phases(self) -> t.List[dict]:
-        return self.data['kill_chain_phases']
+    def kill_chain_phases(self) -> t.Optional[t.List[dict]]:
+        return self.raw_data.get('kill_chain_phases')
     
     @property
-    def description(self) -> str:
-        return self.data['description']
+    def description(self) -> t.Optional[str]:
+        return self.raw_data.get('description')
     
     @property
-    def mitre_data_sources(self) -> t.List[str]:
-        return self.data['x_mitre_data_sources']
+    def mitre_data_sources(self) -> t.Optional[t.List[str]]:
+        return self.raw_data.get('x_mitre_data_sources')
     
     @property
-    def created(self) -> datetime:
-        return datetime.strptime(self.data['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
+    def created(self) -> t.Optional[datetime]:
+        if self.raw_data.get('created'):
+            return datetime.strptime(self.raw_data['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
